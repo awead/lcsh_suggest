d5d6891866bb020e99946caa603e72d8054292a5
Adam Wead <amsterdamos@gmail.com>
2013-06-06 13:29:08 -0400

    Installing rspec

--- lcsh_suggest.gemspec
+++ lcsh_suggest.gemspec
@@ -7,16 +7,21 @@ require "lcsh_suggest/version"
 Gem::Specification.new do |s|
   s.name        = "lcsh_suggest"
   s.version     = LcshSuggest::VERSION
-  s.authors     = ["TODO: Your name"]
-  s.email       = ["TODO: Your email"]
-  s.homepage    = "TODO"
-  s.summary     = "TODO: Summary of LcshSuggest."
-  s.description = "TODO: Description of LcshSuggest."
+  s.authors     = ["Adam Wead"]
+  s.email       = ["amsterdamos@gmail.com"]
+  s.homepage    = "https://github.com/awead/lcsh_suggest"
+  s.summary     = "Search Libary of Congress' subject headings"
+  s.description = "Queries Library of Congress' subject heading search API and returns results in JSON."
 
   s.files = Dir["{app,config,db,lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.rdoc"]
+  s.test_files = Dir["spec/**/*"]
 
   s.add_dependency "rails", "~> 3.2.13"
+  s.add_dependency "curb"
   # s.add_dependency "jquery-rails"
 
   s.add_development_dependency "sqlite3"
+  s.add_development_dependency 'rspec-rails'
+  s.add_development_dependency 'capybara'
+  s.add_development_dependency 'factory_girl_rails'
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,32 @@
+# Created from rails g rspec:install
+# Modified according to: http://viget.com/extend/rails-engine-testing-with-rspec-capybara-and-factorygirl
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../dummy/config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'factory_girl_rails'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = "random"
+end
